826d034c3b29022a2f7844da48e5102c9fa4cf01
haile <hailetechlover@gmail.com>
2024-09-21 07:32:47 +0300

    removing react map from packag

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import WorldMap from "react-world-map";
 import Images from "../assets/images"; // Ensure this correctly imports the image paths
 
 const Landing = () => {
